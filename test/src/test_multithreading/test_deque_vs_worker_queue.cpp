6cf79d574c7ccbe0580964dab463499cfed6ba49
root <root@LAPTOP-P7UDNGNK>
2025-03-14 15:44:41 +0000

    deque vs worker_queue

--- test/src/test_multithreading/test_deque_vs_worker_queue.cpp
+++ test/src/test_multithreading/test_deque_vs_worker_queue.cpp
@@ -26,18 +26,6 @@ class Worker_queue_deque{
             int size_; 
             std::mutex m;
         public:
-            // default constructor credo poi da associare a metodo resize()
-            Worker_queue_deque();
-            // construct whit size of queue_=n;
-            Worker_queue_deque(int n){
-                queue_.resize(n);
-                size_ = n;
-            }
-
-            // resize() di queue_ super easy for the moment
-            void resize(int n){
-                queue_ = std::deque<value_type> (n);
-            }
 
             // void per pigrizia tanto solo per test
             void push_front(value_type t){
@@ -87,9 +75,9 @@ class Worker_queue_deque{
 int main(){
     int size_coda= 100;
     fdapde::Worker_queue<int> q1(size_coda);
-    Worker_queue_deque<int> d1(size_coda);
+    Worker_queue_deque<int> d1;
 
-    
+    //push_front()
     auto start = std::chrono::high_resolution_clock::now();
     for(int j=0; j<size_coda-1; j++){
         q1.push_front(j);
@@ -107,6 +95,76 @@ int main(){
     auto end1 = std::chrono::high_resolution_clock::now();
     auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(end1 - start1);  
     std::cout<<"push_frot deque di n_elementi: "<<size_coda<<" impiegato:"<<duration1.count()<< " microsecondi\n";
+
+    //q1.print();
+    //d1.print();
+
+    //pop_front()
+    auto start2 = std::chrono::high_resolution_clock::now();
+    for(int j=0; j<size_coda-1; j++){
+        q1.pop_front();
+    }
+
+    auto end2 = std::chrono::high_resolution_clock::now();
+    auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2);  
+    std::cout<<"pop_frot worker_queue di n_elementi: "<<size_coda<<" impiegato:"<<duration2.count()<< " microsecondi\n";
+
+    auto start3 = std::chrono::high_resolution_clock::now();
+    for(int j=0; j<size_coda-1; j++){
+        d1.pop_front();
+    }
+
+    auto end3 = std::chrono::high_resolution_clock::now();
+    auto duration3 = std::chrono::duration_cast<std::chrono::microseconds>(end3 - start3);  
+    std::cout<<"pop_frot deque di n_elementi: "<<size_coda<<" impiegato:"<<duration3.count()<< " microsecondi\n";
+
+    //q1.print();
+    //d1.print();
+
+    //push_back()
+    auto start5 = std::chrono::high_resolution_clock::now();
+    for(int j=0; j<size_coda-1; j++){
+        q1.push_back(j);
+    }
+
+    auto end5 = std::chrono::high_resolution_clock::now();
+    auto duration5 = std::chrono::duration_cast<std::chrono::microseconds>(end5 - start5);  
+    std::cout<<"push_back() worker_queue di n_elementi: "<<size_coda<<" impiegato:"<<duration5.count()<< " microsecondi\n";
+
+    auto start6 = std::chrono::high_resolution_clock::now();
+    for(int j=0; j<size_coda-1; j++){
+        d1.push_back(j);
+    }
+
+    auto end6 = std::chrono::high_resolution_clock::now();
+    auto duration6 = std::chrono::duration_cast<std::chrono::microseconds>(end6 - start6);  
+    std::cout<<"push_back() deque di n_elementi: "<<size_coda<<" impiegato:"<<duration6.count()<< " microsecondi\n";
+
+    //q1.print();
+    //d1.print();
+
+    //pop_back()
+    auto start7 = std::chrono::high_resolution_clock::now();
+    for(int j=0; j<size_coda-1; j++){
+        q1.pop_back();
+    }
+
+    auto end7 = std::chrono::high_resolution_clock::now();
+    auto duration7 = std::chrono::duration_cast<std::chrono::microseconds>(end7 - start7);  
+    std::cout<<"pop_back() worker_queue di n_elementi: "<<size_coda<<" impiegato:"<<duration7.count()<< " microsecondi\n";
+
+    auto start8 = std::chrono::high_resolution_clock::now();
+    for(int j=0; j<size_coda-1; j++){
+        d1.pop_back();
+    }
+
+    auto end8 = std::chrono::high_resolution_clock::now();
+    auto duration8 = std::chrono::duration_cast<std::chrono::microseconds>(end8 - start8);  
+    std::cout<<"pop_back() deque di n_elementi: "<<size_coda<<" impiegato:"<<duration8.count()<< " microsecondi\n";
+
+    //q1.print();
+    //d1.print();
+
     return 0;
 }
 
@@ -114,8 +172,15 @@ int main(){
 
 
 /* RISULTATI */
-/*push_frot worker_queue di n_elementi: 100 impiegato:1 microsecondi
-  push_frot deque di n_elementi: 100 impiegato:6 microsecondi
+/*
+push_frot worker_queue di n_elementi: 100 impiegato:1 microsecondi
+push_frot deque di n_elementi: 100 impiegato:4 microsecondi
+pop_frot worker_queue di n_elementi: 100 impiegato:2 microsecondi
+pop_frot deque di n_elementi: 100 impiegato:7 microsecondi
+push_back() worker_queue di n_elementi: 100 impiegato:18 microsecondi
+push_back() deque di n_elementi: 100 impiegato:15 microsecondi
+pop_back() worker_queue di n_elementi: 100 impiegato:12 microsecondi
+pop_back() deque di n_elementi: 100 impiegato:26 microsecondi
 */
 
 
--- test/src/test_multithreading/test_pop_push.cpp
+++ test/src/test_multithreading/test_pop_push.cpp
@@ -15,7 +15,6 @@
  // along with this program.  If not, see <http://www.gnu.org/licenses/>.
  
  #include<fdaPDE/multithreading.h>
- #include<iostream>  //per debug momentaneo
  
  int main(){
      fdapde::Worker_queue<int> q(10);
