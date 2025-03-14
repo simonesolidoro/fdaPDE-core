5fb0a6947b83252c7ccd89a2be103e2452a13502
root <root@LAPTOP-P7UDNGNK>
2025-03-14 16:28:25 +0000

    test concurrency

--- test/src/test_multithreading/test_pop_push_multithread.cpp
+++ test/src/test_multithreading/test_pop_push_multithread.cpp
@@ -17,67 +17,38 @@
 #include<fdaPDE/multithreading.h>
 #include<iostream>  //per debug momentaneo
 
-int size_coda=100000000;
-void padronecodatutta(fdapde::Worker_queue<int> & coda){
-    int ii = size_coda;
-    while(ii!=0){
-        coda.pop_front();
-        ii--;
-    }
-}
-
-void padronecodamulti(fdapde::Worker_queue<int> & coda){
-    int ii = size_coda/2;
-    while(ii!=0){
-        coda.pop_front();
-        ii--;
-    }
-}
-
-void rubalavoro(fdapde::Worker_queue<int> & coda){
-    int ii = size_coda/4;
-    while(ii!=0){
-        coda.pop_back();
-        ii--;
+void pushbackconc(std::vector<int> V, fdapde::Worker_queue<int> & q){
+    for (auto x: V){
+        q.push_back(x);
     }
 }
 
 int main(){
+    int size_coda=10;
     fdapde::Worker_queue<int> q1(size_coda);
-    //popolo coda
-    for(int j=0; j<size_coda-1; j++){
-        q1.push_front(j);
-    }
-    //q1.print();
-
-    auto start = std::chrono::high_resolution_clock::now();
-    std::thread t1(padronecodamulti, std::ref(q1));
-    std::thread t2(rubalavoro, std::ref(q1));
-    std::thread t3(rubalavoro, std::ref(q1));
-
-
+    
+/*
+    // push_back concorrente semplice
+    int a=3;
+    int b=2;
+    std::thread t1([&](){q1.push_back(a);});
+    std::thread t2([&](){q1.push_back(b);});
     t1.join();
     t2.join();
+    q1.print();
+
+    // pop_back concorrente semplice
+    std::thread t3([&q1](){q1.pop_back();});
+    std::thread t4([&q1](){q1.pop_back();});
     t3.join();
-    //q1.print();
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);  
-    std::cout<<"svuotare coda di n_elementi: "<<size_coda<<" con multithrading impiegato:"<<duration.count()<< " microsecondi\n";
-
-    fdapde::Worker_queue<int> q2(size_coda);
-    //popolo coda
-    for(int j=0; j<size_coda-1; j++){
-        q2.push_front(j);
-    }
-
-    auto start2 = std::chrono::high_resolution_clock::now();
-
-    std::thread t4(padronecodatutta, std::ref(q2));
     t4.join();
-    
-    auto end2 = std::chrono::high_resolution_clock::now();
-    auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2);  
-    std::cout<<"svuotare coda di n_elementi: "<<size_coda<<" con 1 thrad impiegato:"<<duration2.count()<< " microsecondi\n";
-
+    q1.print();
+*/
+    std::vector<int> v={1,2,3,4,5};
+    std::thread t1(pushbackconc,v,std::ref(q1));
+    std::thread t2(pushbackconc,v,std::ref(q1));
+    t1.join();
+    t2.join();
+    q1.print();
     return 0;
 }
