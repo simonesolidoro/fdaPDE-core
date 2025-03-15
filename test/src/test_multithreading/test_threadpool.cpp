9328d145951fe7e59c75c1bfaf6b1bd1903b76b3
root <root@LAPTOP-P7UDNGNK>
2025-03-15 14:11:12 +0000

    test per verifica efficenza di piu worker_queue vs singola coda

--- test/src/test_multithreading/test_threadpool.cpp
+++ test/src/test_multithreading/test_threadpool.cpp
@@ -16,8 +16,14 @@
 
 #include<fdaPDE/multithreading.h>
 
-int size_coda=10;
+
+/*TEST singola coda thread-safe  vs  piu worker_queue */
+
+//OSS: in realta servirebbe threadpool che implementa logica per cui un thread finito la sua worker_queue va a pescare da altre, quindi non so quanto utile questo test
+
+int size_coda=1000;
 using namespace std::chrono_literals;
+
 void padronecoda(fdapde::Worker_queue<int> & coda){
     while(! coda.empty()){
         coda.pop_front();
@@ -25,7 +31,6 @@ void padronecoda(fdapde::Worker_queue<int> & coda){
     }
 
 }
-
 
 
 void rubalavoro(fdapde::Worker_queue<int> & coda1,fdapde::Worker_queue<int> & coda2){
@@ -38,57 +43,149 @@ void rubalavoro(fdapde::Worker_queue<int> & coda1,fdapde::Worker_queue<int> & co
         //std::this_thread::sleep_for(1ms);
     }
 }
-    
+
+template <typename T>
+class Unica_coda_threadsafe{
+    private:
+        std::deque<T> queue_; 
+        std::mutex m_;
+    public:
+
+    // tutti membri pop push con lock (thtread-safe)
+        void push_front(T t){
+            std::lock_guard<std::mutex> loc(m_);
+            queue_.push_back(t); 
+        }
+
+        T pop_front(){
+            std::lock_guard<std::mutex> loc(m_);
+            T ret=queue_.back();
+            queue_.pop_back();
+            return ret;   
+        }
+                 
+        void push_back(T t){
+            std::lock_guard<std::mutex> loc(m_);
+            queue_.push_front(t);  
+        }
+
+        T pop_back(){
+            std::lock_guard<std::mutex> loc(m_);
+            T ret=queue_.front();
+            queue_.pop_front();
+            return ret;
+        }
+
+        // wrap of function size() empty() of vector thrade-safe
+        int size(){
+            std::lock_guard<std::mutex> loc(m_);
+            return queue_.size();
+        }
+        bool empty(){
+            std::lock_guard<std::mutex> loc(m_);
+            return queue_.empty();
+        }
+        //per debug momentanei
+        void print(){
+            for (T i : queue_)
+                std::cout<<i<<"  ";
+            std::cout<<std::endl;
+        }
+    };
+
+
 
 int main(){
 
     fdapde::Worker_queue<int> q1(size_coda);
     fdapde::Worker_queue<int> q2(size_coda);
 
-    //popolo coda
+    //popolo code
     for(int j=1; j<=size_coda; j++){
         q1.push_front(j);
     }
     for(int j=1; j<=size_coda; j++){
         q2.push_front(j);
     }
-    q1.print();
-    q2.print();
+    //q1.print();
+    //q2.print();
 
-    
+    // svuoto con tre thread due che hanno loro coda e uno che ruba dal fondo 
+    auto start1 = std::chrono::high_resolution_clock::now();
     std::thread t1(padronecoda, std::ref(q1));
     std::thread t2(padronecoda, std::ref(q2));
-    //std::thread t3(rubalavoro, std::ref(q1),std::ref(q2));
+    std::thread t3(rubalavoro, std::ref(q1),std::ref(q2));
 
 
     t1.join();
     t2.join();
-    //t3.join();
-    q1.print();
-    q2.print();
+    t3.join();
+    //q1.print();
+    //q2.print();
+    auto end1 = std::chrono::high_resolution_clock::now();
+    auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(end1 - start1);  
+    std::cout<<"svuotare 2 singole coda di n_elementi: "<<size_coda<<" con multithrading impiegato:"<<duration1.count()<< " microsecondi\n";
 
-/*  
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);  
-    std::cout<<"svuotare coda di n_elementi: "<<size_coda<<" con multithrading impiegato:"<<duration.count()<< " microsecondi\n";
 
-    fdapde::Worker_queue<int> q2(size_coda);
-    //popolo coda
-    for(int j=0; j<size_coda-1; j++){
-        q2.push_front(j);
+    /* unica coda per tutti i thread */
+    //popolo
+    Unica_coda_threadsafe<int> D;
+    for (int i =1; i<= size_coda*2; i++){
+        D.push_back(i);
     }
 
+    // svuoto coda unica con tre thread
     auto start2 = std::chrono::high_resolution_clock::now();
+    std::thread tt([&D](){while (!D.empty()){
+                                D.pop_back();
+                                //std::this_thread::sleep_for(1ms);
+                                 }
+                        }
+                    );
+    std::thread tt1([&D](){while (!D.empty()){
+                                D.pop_back();
+                                //std::this_thread::sleep_for(1ms);
+                                 }
+                        }
+                    );   
+    std::thread tt2([&D](){while (!D.empty()){
+                                D.pop_back();
+                                //std::this_thread::sleep_for(1ms);
+                                 }
+                        }
+                    );
 
-    std::thread t4(padronecodatutta, std::ref(q2));
-    t4.join();
-    
+    tt.join();
+    tt1.join();
+    tt2.join();
+
     auto end2 = std::chrono::high_resolution_clock::now();
     auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2);  
-    std::cout<<"svuotare coda di n_elementi: "<<size_coda<<" con 1 thrad impiegato:"<<duration2.count()<< " microsecondi\n";
-*/
+    std::cout<<"svuotare singola coda di n_elementi: "<<size_coda*2<<" con multithrading impiegato:"<<duration2.count()<< " microsecondi\n";
+
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*test multithreading giocattolo
