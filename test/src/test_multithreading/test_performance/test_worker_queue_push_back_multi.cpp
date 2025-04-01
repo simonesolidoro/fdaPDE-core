7c67309f64e73f6f35ddeaffa38fcf32cdaa32e1
simonesolidoro <root@LAPTOP-P7UDNGNK>
2025-04-01 15:59:29 +0000

    test multithread worker_queue

---
+++ test/src/test_multithreading/test_performance/test_worker_queue_push_back_multi.cpp
@@ -0,0 +1,59 @@
+// This file is part of fdaPDE, a C++ library for physics-informed
+// spatial and functional data analysis.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include<fdaPDE/multithreading.h>
+
+
+using value = std::string;
+
+
+// push_back di n elementi per worker queue 
+void push_back_di_n_elem(fdapde::Worker_queue<value> & q,int n, value el){
+    for (int j=0; j<n; j++){
+        q.push_back(el);
+    }
+};
+
+
+int main(){
+    int size_coda= 16000;
+    int n_thread = 8;
+    int n_singolo= size_coda / n_thread;
+
+    fdapde::Worker_queue<value> q1(size_coda);
+    value el = "ciao";
+
+    //push_back() multithreading
+   fdapde::Worker_queue<value> q(size_coda);
+
+    auto start = std::chrono::high_resolution_clock::now();  
+    //worker_queue push_back parallelo
+    std::vector<std::thread> thread_pool;
+    for (int j=0; j<n_thread; j++){
+        thread_pool.emplace_back(push_back_di_n_elem,std::ref(q),n_singolo, el);
+    }
+
+    for (int k= 0; k<n_thread; k++){
+        thread_pool[k].join();
+    }
+    //q1.print(); //per debug
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);  
+    //std::cout<<"push_back in worker_queue di n_elementi: "<<size_coda<<" con n_thread:"<<n_thread<<" impiegato:"<<duration.count()<< " microsecondi\n";
+    std::cout<<duration.count()<<",";
+    return 0;
+}
+
