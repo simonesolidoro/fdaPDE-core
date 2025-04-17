60732582456d7752fa26e431568a97c34f9ee0bf
simonesolidoro <root@LAPTOP-P7UDNGNK>
2025-04-17 02:03:25 +0000

    scommentato include deque per test performance, refactoring version compila :)

--- fdaPDE/multithreading.h
+++ fdaPDE/multithreading.h
@@ -38,7 +38,7 @@
 // per nuove code parzialmente specializzate
 //#include "src/multithreading/synchro_queue_2.h"
 //#include "src/multithreading/Synchro_queue_wait_2.h"
-//#include "src/multithreading/deque.h"
+#include "src/multithreading/deque.h"
 #include "src/multithreading/Synchro_queue_3.h"
 // clang-format on
 
