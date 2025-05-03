1c3c78d56e1b1c82e9e3d221e344e7714c274d8e
simosoli <simone1.solidoro@mail.polimi.it>
2025-05-03 10:03:05 +0000

    spostata classe Worker dentro classe Threadpool

--- fdaPDE/src/multithreading/threadpool.h
+++ fdaPDE/src/multithreading/threadpool.h
@@ -114,6 +114,82 @@ namespace fdapde{
             int n_worker_;
             indx_worker indxw_; 
         public:
+            class Worker{
+                private: 
+                    fdapde::Synchro_queue<job,fdapde::relax_nowait> sync_queue_;
+                    std::thread t_;
+                    bool stop_ = false;
+                    std::mutex m_;
+                    std::condition_variable cv_; 
+                public:
+                    // costruttore con numero elementi di coda
+                    Worker(int n):sync_queue_(n),t_(&Worker::worker_loop,this){};
+                    
+                    ~Worker(){
+                        //while(!sync_queue_.empty()){}; //per aspettare che worker finisca i job in coda. PB: a volte empty() chiamato prima di push_back() di metodo send_task di threadpool
+                        std::unique_lock<std::mutex> loc(m_);
+                        stop_ = true; //PROBLEMA: chiamato distruttore prima che job effettivamente finiti. SOLUZIONE: usare future associato a task in main cosi che future.get() garantisce fine di task prima di chiamata distruttore
+                        //DOMANDA: serve che stop_ sia atomico ? bisogna distruggere dento al mutex ? perche non è possibile che notifca arrivi a worker_loop si sveglia e controlla stop_ prima che sia effettivamente cambiato
+                        //CREDO SERVA loc di MUTEX !!!!!!!!!!!!
+                        //oss: stop atomico inutile, o serve mutex e quindi modifica stop dentro mutex quindi non serve sia atomico, o non serve niente basta il notify (come in threadpool di link di primisimo esempio visto) 
+                        cv_.notify_one();
+                        loc.unlock();
+                        t_.join();
+                    }
+                    //per poter bloccare il mutex di Worker m_ in threadpool
+                    std::unique_lock<std::mutex> get_loc(){
+                    std::unique_lock<std::mutex> loc(m_);
+                    return loc;          
+                    }
+                    //per poter notificare da threadpool
+                    void notifica(){
+                        cv_.notify_one();
+                    }
+                    void worker_loop(){
+                        while(!stop_){
+                            //TODO: capire come mettere a dormire se coda vuota, nel frattempo messo yields()
+                            //OSS: uso di empty() brutto ma non vanifica del tutto synchro_queue perche blocca tutto mentre lo fa ma poi pop e push semi sequenziali.
+                            //     il problema è l'uso di un mutex necessario per usare una cv che renderebbe tutto sequenziale e vanifica tutto lavoro fatto fin ora(sync_queue sarebbe inutile tanto varebbe usare normale deque)
+                            //OSS: possibile mettere un  mutex che avvisi e poi fare pop fuori da mutex, push pero deve essere fatto dentro per avere certezza che notify() sia coerente 
+                            //OSS: alternativa thread non va mai a dormire, se fa il pop di un nullopt va a rubare job ad altri (credo lo faccia eigen cosi)
+                            std::unique_lock<std::mutex> loc(m_);
+                            cv_.wait(loc,[this](){return !sync_queue_.empty() || stop_;});
+                            loc.unlock();
+                            //OSS: cosi facendo durante un push da threadpool non possono avvenire pop (sequenziale :( ), ma rimane che durante un pop possono essere fatti dei push. si perde a meta il vantaggio di avere synchro_queue parzialmente non bloking, ma non del tutto
+                            std::optional<job> j = sync_queue_.pop_front();
+                            if(j){//esegue se non è nullopt
+                                (j.value())(); //esegue funzioni con 0 parametri e void. per non void si dovra fare wrap e associare a promise. per parametri lamda wrap che li cattura cosi no param  
+                            }
+                            /*
+                            else{
+                                std::this_thread::yield(); //da controllo a OS, possibile che sospenda l'esecuzione del thread a favore di altro, (usato per mettere una pezza a mancanza condion varibale che fa wait se coda empty)
+                            } */                             
+                        }
+                    };
+
+                    //wrap di funzioni per pop e push
+                    bool push_front(job fun){
+                        return  sync_queue_.push_front(fun);
+                    };
+                    bool push_back(job fun){
+                        return  sync_queue_.push_front(fun);
+                    };
+                    std::optional<job> pop_front(){
+                        return  sync_queue_.pop_front();
+                    };
+                    std::optional<job> pop_back(){
+                        return  sync_queue_.pop_back();
+                    };
+
+                    //calcolo elementi in coda (non affidabile ma tanto solo indicativo)
+                    int count_el (){ //OSS: problema che se h == t rida coda vuota ma magari coda piena. ci pensiamo poi a come risolvere
+                        int t = sync_queue_.get_tail();
+                        int h = sync_queue_.get_head();
+                        int tmp;
+                        (h>t)? (tmp= h-t):(tmp= t-h);
+                        return tmp;
+                    };
+            };
             //n = size code, k = numero worker
             Threadpool(int n, int k):n_worker_(k){
                 threadpool_.reserve(k);
