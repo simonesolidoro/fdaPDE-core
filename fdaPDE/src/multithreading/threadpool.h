8c9e8110c03538d887b65972801bd39f91f50193
simosoli <simone1.solidoro@mail.polimi.it>
2025-05-03 11:12:35 +0000

    quando spostata classe worker dentro threadpool avevo dimenticato di eliminare definizione di classe fuori, ora sistemato

--- fdaPDE/src/multithreading/threadpool.h
+++ fdaPDE/src/multithreading/threadpool.h
@@ -19,85 +19,6 @@
 #include "header_check.h"
 
 namespace fdapde{
-    template<typename T,typename... Args> //per ora Args... inutile perche passate funzioni con parametri come lambda senza parametri: tipo [fun,args](){fun(args);}
-    class Worker{
-        using job = std::function<T(Args... args)>;
-        private: 
-            fdapde::Synchro_queue<job,fdapde::relax_nowait> sync_queue_;
-            std::thread t_;
-            bool stop_ = false;
-            std::mutex m_;
-            std::condition_variable cv_; 
-        public:
-            // costruttore con numero elementi di coda
-            Worker(int n):sync_queue_(n),t_(&Worker::worker_loop,this){};
-            
-            ~Worker(){
-                //while(!sync_queue_.empty()){}; //per aspettare che worker finisca i job in coda. PB: a volte empty() chiamato prima di push_back() di metodo send_task di threadpool
-                std::unique_lock<std::mutex> loc(m_);
-                stop_ = true; //PROBLEMA: chiamato distruttore prima che job effettivamente finiti. SOLUZIONE: usare future associato a task in main cosi che future.get() garantisce fine di task prima di chiamata distruttore
-                //DOMANDA: serve che stop_ sia atomico ? bisogna distruggere dento al mutex ? perche non è possibile che notifca arrivi a worker_loop si sveglia e controlla stop_ prima che sia effettivamente cambiato
-                //CREDO SERVA loc di MUTEX !!!!!!!!!!!!
-                //oss: stop atomico inutile, o serve mutex e quindi modifica stop dentro mutex quindi non serve sia atomico, o non serve niente basta il notify (come in threadpool di link di primisimo esempio visto) 
-                cv_.notify_one();
-                loc.unlock();
-                t_.join();
-            }
-            //per poter bloccare il mutex di Worker m_ in threadpool
-            std::unique_lock<std::mutex> get_loc(){
-               std::unique_lock<std::mutex> loc(m_);
-               return loc;          
-            }
-            //per poter notificare da threadpool
-            void notifica(){
-                cv_.notify_one();
-            }
-            void worker_loop(){
-                while(!stop_){
-                    //TODO: capire come mettere a dormire se coda vuota, nel frattempo messo yields()
-                    //OSS: uso di empty() brutto ma non vanifica del tutto synchro_queue perche blocca tutto mentre lo fa ma poi pop e push semi sequenziali.
-                    //     il problema è l'uso di un mutex necessario per usare una cv che renderebbe tutto sequenziale e vanifica tutto lavoro fatto fin ora(sync_queue sarebbe inutile tanto varebbe usare normale deque)
-                    //OSS: possibile mettere un  mutex che avvisi e poi fare pop fuori da mutex, push pero deve essere fatto dentro per avere certezza che notify() sia coerente 
-                    //OSS: alternativa thread non va mai a dormire, se fa il pop di un nullopt va a rubare job ad altri (credo lo faccia eigen cosi)
-                    std::unique_lock<std::mutex> loc(m_);
-                    cv_.wait(loc,[this](){return !sync_queue_.empty() || stop_;});
-                    loc.unlock();
-                    //OSS: cosi facendo durante un push da threadpool non possono avvenire pop (sequenziale :( ), ma rimane che durante un pop possono essere fatti dei push. si perde a meta il vantaggio di avere synchro_queue parzialmente non bloking, ma non del tutto
-                    std::optional<job> j = sync_queue_.pop_front();
-                    if(j){//esegue se non è nullopt
-                        (j.value())(); //esegue funzioni con 0 parametri e void. per non void si dovra fare wrap e associare a promise. per parametri lamda wrap che li cattura cosi no param  
-                    }
-                    /*
-                    else{
-                        std::this_thread::yield(); //da controllo a OS, possibile che sospenda l'esecuzione del thread a favore di altro, (usato per mettere una pezza a mancanza condion varibale che fa wait se coda empty)
-                    } */                             
-                }
-            };
-
-            //wrap di funzioni per pop e push
-            bool push_front(job fun){
-                return  sync_queue_.push_front(fun);
-            };
-            bool push_back(job fun){
-                return  sync_queue_.push_front(fun);
-            };
-            std::optional<job> pop_front(){
-                return  sync_queue_.pop_front();
-            };
-            std::optional<job> pop_back(){
-                return  sync_queue_.pop_back();
-            };
-
-            //calcolo elementi in coda (non affidabile ma tanto solo indicativo)
-            int count_el (){ //OSS: problema che se h == t rida coda vuota ma magari coda piena. ci pensiamo poi a come risolvere
-                int t = sync_queue_.get_tail();
-                int h = sync_queue_.get_head();
-                int tmp;
-                (h>t)? (tmp= h-t):(tmp= t-h);
-                return tmp;
-            };
-    };
-
     template<typename T,typename... Args>
     class Threadpool{
         using job = std::function<T(Args... args)>;
@@ -108,11 +29,6 @@ namespace fdapde{
                 (indx_ == n_worker-1)? (indx_ = 0):(indx_++);
             };
         };
-        private:
-            std::vector<std::shared_ptr<fdapde::Worker<T,Args...>>> threadpool_; //vettore di putatori perche non movable e copiable synchro_queue per via di mutex
-            //std::vector<int> count_task_; //TODO: probabimente meglio togliere da qua e associare ad ogni worker il suo contatore cosi che potra decrementarlo in worker_loop. OSS: lettura di contatore non sara affidabile perche nel mentre che leggi potrebbe cambiare (es leggo il primo che ha 10 job ora che vado a leggere ultimo magari primo ha finto, però se cosi fosse i job sarebbero piu veloci di scorrere un vettore e confrontare due valori e quindi che senso ha parallelizzarli)
-            int n_worker_;
-            indx_worker indxw_; 
         public:
             class Worker{
                 private: 
@@ -121,6 +37,7 @@ namespace fdapde{
                     bool stop_ = false;
                     std::mutex m_;
                     std::condition_variable cv_; 
+                    int count_job_ = 0;
                 public:
                     // costruttore con numero elementi di coda
                     Worker(int n):sync_queue_(n),t_(&Worker::worker_loop,this){};
@@ -156,7 +73,7 @@ namespace fdapde{
                             cv_.wait(loc,[this](){return !sync_queue_.empty() || stop_;});
                             loc.unlock();
                             //OSS: cosi facendo durante un push da threadpool non possono avvenire pop (sequenziale :( ), ma rimane che durante un pop possono essere fatti dei push. si perde a meta il vantaggio di avere synchro_queue parzialmente non bloking, ma non del tutto
-                            std::optional<job> j = sync_queue_.pop_front();
+                            std::optional<job> j = pop_front();
                             if(j){//esegue se non è nullopt
                                 (j.value())(); //esegue funzioni con 0 parametri e void. per non void si dovra fare wrap e associare a promise. per parametri lamda wrap che li cattura cosi no param  
                             }
@@ -166,21 +83,31 @@ namespace fdapde{
                             } */                             
                         }
                     };
+                    
+                    int get_count_job() const{
+                        return count_job_;
+                    };
 
                     //wrap di funzioni per pop e push. //OSS: se count_el non si sistema aggiungere modifica a contatori di numero job in coda direttamente in questi wrap di push e pop
                     bool push_front(job fun){
+                        count_job_ ++;
                         return  sync_queue_.push_front(fun);
                     };
                     bool push_back(job fun){
+                        count_job_ ++;
                         return  sync_queue_.push_front(fun);
                     };
                     std::optional<job> pop_front(){
+                        count_job_ --;
                         return  sync_queue_.pop_front();
                     };
                     std::optional<job> pop_back(){
+                        count_job_ --;
                         return  sync_queue_.pop_back();
                     };
 
+
+            /* sostituito da count_job_
                     //calcolo elementi in coda (non affidabile ma tanto solo indicativo)
                     //TODO: meglio spostare questa funzzione come membro di Synchro_queue direttamente
                     int count_el (){ //TODO: problema che se h == t rida coda vuota ma magari coda piena (molto grave perche canditato migliore (cioe quello che si crede sia il piu libero), in realta è il peggiore). ci pensiamo poi a come risolvere
@@ -193,12 +120,19 @@ namespace fdapde{
                         (h>t)? (tmp= h-t):(tmp= size_queue-(t-h)); // se h<t allora t-h sono spazi vuoti
                         return tmp;
                     };
+            */
             };
+        private:
+            std::vector<std::shared_ptr<Worker>> threadpool_; //vettore di putatori perche non movable e copiable synchro_queue per via di mutex
+            //std::vector<int> count_task_; //TODO: probabimente meglio togliere da qua e associare ad ogni worker il suo contatore cosi che potra decrementarlo in worker_loop. OSS: lettura di contatore non sara affidabile perche nel mentre che leggi potrebbe cambiare (es leggo il primo che ha 10 job ora che vado a leggere ultimo magari primo ha finto, però se cosi fosse i job sarebbero piu veloci di scorrere un vettore e confrontare due valori e quindi che senso ha parallelizzarli)
+            int n_worker_;
+            indx_worker indxw_; 
+        public:
             //n = size code, k = numero worker
             Threadpool(int n, int k):n_worker_(k){
                 threadpool_.reserve(k);
                 for(int i=0; i<k; i++){
-                    threadpool_.emplace_back(std::make_shared<fdapde::Worker<T,Args...>> (n));
+                    threadpool_.emplace_back(std::make_shared<Worker> (n));
                     //count_task_.push_back(0);
                 }
             };
@@ -206,9 +140,9 @@ namespace fdapde{
             //ridà indice di worker piu libero (lettura di elmenti in sync_queue di worker fatta con metodo count_el non affidabile ma è abbastanza per avere un idea)
             int indx_most_free(){
                 int worker_indx = 0;
-                int min_elem= threadpool_[0]->count_el(); //numero elementi in primo worker 
+                int min_elem= threadpool_[0]->get_count_job(); //numero elementi in primo worker 
                 for (int j=1; j<n_worker_; j++){
-                    int current_el = threadpool_[j]->count_el();
+                    int current_el = threadpool_[j]->get_count_job();
                     if(current_el < min_elem ){ 
                         worker_indx = j;
                         min_elem = current_el;
@@ -219,9 +153,9 @@ namespace fdapde{
             // indice di worker con piu job in coda, sara utile per steal job
             int indx_most_busy(){
                 int worker_indx = 0;
-                int max_elem= threadpool_[0]->count_el(); //numero elementi in primo worker 
+                int max_elem= threadpool_[0]->get_count_job(); //numero elementi in primo worker 
                 for (int j=1; j<n_worker_; j++){
-                    int current_el = threadpool_[j]->count_el();
+                    int current_el = threadpool_[j]->get_count_job();
                     if(current_el > max_elem ){
                         worker_indx = j;
                         max_elem = current_el;
