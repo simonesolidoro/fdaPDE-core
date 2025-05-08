a74d0d86e7ca1aae7e84d350259cc4cbdfbdee86
simosoli <simone1.solidoro@mail.polimi.it>
2025-05-08 09:39:35 +0000

    test perfromance threadpool

---
+++ test/src/test_multithreading/test_performance_threadpool/risultati.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#$1 is the name of the file to be ran
+#$2 is the number of times the file is ran
+#$3 is the number of operation in function
+#$4 is the number of threads, if needed
+
+rm $1".txt"
+
+touch $1".txt"
+for i in `seq 1 $2`; do
+    # Esegui il programma e salva l'output nel file
+    ./$1 $3 $4 >>$1".txt"
+done
+
+
