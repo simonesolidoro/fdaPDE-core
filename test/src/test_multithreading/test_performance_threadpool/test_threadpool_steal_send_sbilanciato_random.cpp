3ddb843282483ef8c57351186a2d8966dc34b3e8
gupo33 <100994806+gupo33@users.noreply.github.com>
2025-05-15 11:29:24 +0200

    test random vs most busy steal

---
+++ test/src/test_multithreading/test_performance_threadpool/test_threadpool_steal_send_sbilanciato_random.cpp
@@ -0,0 +1,65 @@
+// This file is part of fdaPDE, a C++ library for physics-informed
+// spatial and functional data analysis.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include<fdaPDE/multithreading.h>
+/*manda prima metà dei job a unico worker e altra metà a metà dei worker, cosi metà worker saranno ladri però job in code sono sbilanciati*/
+void contafino(int n){
+    int a= 0;
+    for (int j = 0; j<n; j++){
+        a++;
+        a--;
+        a++;
+        a--;
+    }
+    //std::this_thread::sleep_for(std::chrono::microseconds(1000));
+
+}
+
+
+int main(int argc, char** argv){
+//manda meta job totali solo a uno e poi atra meta solo a meta worker, cosi si crea asimmetria e starvetion in steal_from_most_busy dovrebbe essere piu evidente e peggiorare prestazioni
+    int n = std::stoi(argv[1]); //numero cicli in contafino // lunghezza singolo job
+
+    int n_thread = std::stoi(argv[2]);
+    int n_job = 100;
+    fdapde::Threadpool<fdapde::steal::random> tp(n_job,n_thread);
+
+    std::vector<std::function<void(int)>> jobs;
+    std::vector<std::optional<std::future<bool>>> futs;
+    for(int i= 0; i<n_job; i++){
+        jobs.push_back(contafino);
+    }
+    
+    auto start2 = std::chrono::high_resolution_clock::now();
+
+    for(int i= 0; i<n_job/2; i++){
+        futs.push_back(std::move(tp.send_task_only_to_zero(contafino,n)));
+    }
+    for(int i= 0; i<n_job/2; i++){
+        futs.push_back(std::move(tp.send_task_only_to_some(contafino,n)));
+    }
+    for(int i= 0; i<n_job; i++){
+        if(futs[i]){
+            futs[i].value().get();
+        }
+    }
+
+    auto end2 = std::chrono::high_resolution_clock::now();
+    auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2);  
+    //std::cout<<"operazioni fatte: "<<n*n_job<<"  con n_job: "<<n_job<<"mandati su n_thread:"<<n_thread<<" impiegato:"<<duration2.count()<< " microsecondi\n";
+    std::cout<<duration2.count()<<",";
+    return 0;
+}
