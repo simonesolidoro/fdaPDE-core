435a657d2d18eb29778758e9ba704a427bfc113d
simo <simone1.solidoro@mail.polimi.it>
2025-07-04 14:59:59 +0000

    test_parallel_for_granularity

---
+++ test/src/test_multithreading/test_cluster_threadpool/test_threadpool_parallel_for_sure_n/iterazioni_bilanciate/run_iterazioni_bilanciate_granularity.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#$1 is the number of times the file is ran
+
+#$2 size range of for
+#$3 pesantezza body function (numero operazioni= $4 * 4)
+#$4 is the number of threads, if needed
+#$5 size queue of worker
+#$6 
+
+
+# n_blocchi = []
+granularity=($6)
+
+# Per ogni eseguibile nella lista
+for nb in "${granularity[@]}"; do
+    # Crea (o sovrascrive) il file di output
+    output_file="test_parallel_for_sure_gran_${nb}.txt"
+    #rm -f "$output_file"
+    touch "$output_file"
+
+    # Esegui il programma N volte e salva l'output
+    for i in $(seq 1 "$1"); do
+        ./"test_parallel_for_granularity" "$2" "$3" "$4" "$5" "$nb">> "$output_file" 
+        #sleep 0.1  # evita congestione
+    done
+    #sleep 0.1  # evita congestione
+done
