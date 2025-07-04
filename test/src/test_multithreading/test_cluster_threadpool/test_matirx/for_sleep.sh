77ba336725621b36f50aae4ed695eeb0ef35d66a
simo <simone1.solidoro@mail.polimi.it>
2025-07-04 15:00:34 +0000

    test somma di matrici in parallelo

---
+++ test/src/test_multithreading/test_cluster_threadpool/test_matirx/for_sleep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#$1 is the number of times the file is ran
+
+#$2 size of nxn matrix
+
+
+# test_for.txt 
+#rm -f "test_for.txt"
+touch "test_for_matrix_sleep.txt"
+
+# Esegui il programma N volte e salva l'output
+for i in $(seq 1 "$1"); do
+    ./"test_for_matrix_sleep" "$2" >> "test_for_matrix_sleep.txt" 
+done
