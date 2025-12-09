dff2c85119a75f64ef869876e1dfa66e4529becf
simo <simone1.solidoro@mail.polimi.it>
2025-12-09 10:13:43 +0000

    pulizia reduce()

--- fdaPDE/src/multithreading/threadpool_2.h
+++ fdaPDE/src/multithreading/threadpool_2.h
@@ -576,37 +576,9 @@ template <typename SchedulingStrategy = round_robin_scheduling,typename Stealing
         return;
     }
 
-    // //reduce
-    // template<typename Iterator,typename F> //F operazione binaria (sum,min,max,dot)
-    // auto reduce(Iterator begin, Iterator end, F&& operation, int granularity = -1){
-    //     using Value_type = decltype(operation(*begin, *begin));
-    //     struct alignas(64) AlignedValue_type {
-    //         Value_type value;
-    //     };
-    //     std::vector<AlignedValue_type> worker_partial_result(n_worker_);
-    //     //inizializzazione worker_partial_result[i].value con primi elementi di range
-    //     int range = end-begin;
-    //     int stop_inizialization = std::min(n_worker_,range);
-    //     for (int i = 0; i<stop_inizialization; ++i){
-    //         worker_partial_result[i].value = *begin;
-    //         ++begin;
-    //     }
 
-    //     //parallelizza range restante dopo inizializzazione (begin ora è uguale a begin_inziale+stop_inizialization)
-    //     this->parallel_for(begin, end,
-    //       [&operation, &worker_partial_result](Iterator i, int index_worker) {
-    //           worker_partial_result[index_worker].value = operation(worker_partial_result[index_worker].value,*i); 
-    //       },
-    //       granularity);
-    //     // final reduce
-    //     Value_type ret = worker_partial_result[0].value;
-    //     for (int i = 1; i<n_worker_; i++){
-    //         ret = operation(ret,worker_partial_result[i].value);
-    //     }
-    //     return ret;
-    // }
 
-        //reduce paralle wrap di std::reduce
+    //reduce parallel: wrap di std::reduce ogni worker applica std::reduce in proprio sotto range e poi reduce finale dei risultati parziali dei worker
     template<typename Iterator,typename Value_type, typename F> //F operazione binaria (sum,min,max,dot)
     Value_type reduce(Iterator begin, Iterator end, Value_type init, F&& operation){
         
