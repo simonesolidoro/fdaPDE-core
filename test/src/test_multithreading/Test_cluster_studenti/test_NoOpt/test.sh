8bef4c6514ddd282cf79bdfbd0f50431318faa1c
simo <simone1.solidoro@mail.polimi.it>
2026-01-05 15:15:39 +0000

    pulita cartella test, lasciato solo funzionamento e performance

--- test/src/test_multithreading/Test_cluster_studenti/test_NoOpt/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-n_threads=(1 2 3 4 5 6 7 8) # 1 lasciato per vedere overhead threadpool rispetto seq 
-output_file="test_noOpt_.txt"
-for i in "${n_threads[@]}"; do
-    ./test "$i" >> "$output_file"
-done
